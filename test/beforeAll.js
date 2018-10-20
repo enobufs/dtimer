c57df5cc35727dfa468993152491c6243605ba4d
enobufs <yt0916@gmail.com>
2018-10-19 23:17:54 -0700

    Delay event emission by a tick

---
+++ test/beforeAll.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var Promise = require('bluebird');
+Promise.config({
+    // Enable warnings
+    warnings: true,
+    // Enable long stack traces
+    longStackTraces: true,
+    // Enable cancellation
+    cancellation: true,
+    // Enable monitoring
+    monitoring: true
+});
